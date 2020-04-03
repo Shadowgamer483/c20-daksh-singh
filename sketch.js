083b502e0a4c94a5b197e682f7936e47cd8fe8f2
Shadowgamer483 <60564162+Shadowgamer483@users.noreply.github.com>
2020-04-03 19:47:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+
+
+function draw() {
+
+  background(rgb(World.mouseY, World.mouseX,World.mouseY))
+  
+fill("red")
+ellipse(World.mouseX,World.mouseY,10,10)  
+noFill()
+fill(rgb(World.mouseY,World.mouseY,World.mouseY))
+textSize("50");
+text("EASY",200,200);
+    
+
+
+}
